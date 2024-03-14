b2a0c298855f9c7cc0d7c58725b2d6d7d495776e
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-03-14 09:51:32 +0530

    Multiple components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
-  const title='Welcome to React';
-  const likes=60;
-  const link="https://google.com";
   return (
     <div className="App">
-      <div className='content'>
-        <h1>{title}</h1>
-        <p>Liked: {likes} times</p>
-        {/* <p>{person}</p>  */}
-        <p>{10}</p>
-        <p>{"Hello Ninjas"}</p>
-        <p>[1,2,3,4,5]</p>
-        <p>{Math.random()*10}</p>
-        <a href={link}>Google</a>
+     <Navbar></Navbar>
+      <div className='Navbar'>
+        <h1>Content</h1>
+
+
       </div>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+const NavBar= () => {
+    return ( 
+        <nav>NavBar
+            <div className="links">
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default NavBar;
