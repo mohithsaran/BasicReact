e6aa6574f374645a161724f286b518dbc7384329
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-03-14 13:00:33 +0530

    Click Events

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="content">
       <Home />
     </div>
-    /</div>
+    </div>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+    const handleClick=(e) => {
+        console.log('Hello Ninjas',e);
+    }
+    const handleClickMeagain=(name,e) => {
+        console.log('Hello  again ' + name, e.target);
+    }
     return ( 
         <div className="Home">
             <h2>HomePage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e)=>{
+               handleClickMeagain('Mohith',e)
+            }}>Click Me again</button>
+
         </div>
      );
 }
